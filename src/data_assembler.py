ae64009fc9859c5188a74ceb098c328127cc70b9
apathriel <gabrielandersen1@gmail.com>
2024-09-14 16:55:38 +0200

    Refactor data assembly script to use pathlib for file handling

--- src/data_assembler.py
+++ src/data_assembler.py
@@ -1,23 +1,23 @@
-import os
+from pathlib import Path
+from glob import glob
 import pandas as pd
 from tqdm import tqdm
 
-def assemble_csv_files(folder_path, output_file):
+def assemble_csv_files(folder_path: Path, output_file: Path) -> None:
     # Get a list of all CSV files in the folder
-    csv_files = [file for file in os.listdir(folder_path) if file.endswith('.csv')]
+    csv_files = [file for file in Path(folder_path).glob('*.csv')]
 
     # Initialize an empty DataFrame to store the combined data
     combined_data = pd.DataFrame()
 
     # Iterate through each CSV file and append its data to the combined DataFrame
     for file in tqdm(csv_files):
-        file_path = os.path.join(folder_path, file)
-        data = pd.read_csv(file_path)
+        data = pd.read_csv(file)
         combined_data = pd.concat([combined_data, data])
 
     # Write the combined data to the output file
     combined_data.to_csv(output_file, index=False)
 
-folder_path = os.path.join('in', 'data_100k_chunks')
-output_file = os.path.join('in', 'all_masters.csv')
+folder_path = Path('in') / 'data_100k_chunks'
+output_file = Path('in') / 'all_masters.csv'
 assemble_csv_files(folder_path, output_file)
